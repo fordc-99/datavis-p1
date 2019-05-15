d926b8622b2cc058746c9e4ef8a7f195dbcc286b
Ashley Wang <43757547+ashleyrwang@users.noreply.github.com>
2019-05-14 19:05:09 -0500

    lint + combine

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import {barVis} from './bar';
 import {bgLake, bgLand} from './bg';
 import {title} from './title';
+import {phaseDiagram} from './phase';
 import {select} from 'd3-selection';
 
 // if the data you are going to import is small, then you can import it using es6 import
 // import MY_DATA from './app/data/example.json'
 // (I tend to think it's best to use screaming snake case for imported json)
 const domReady = require('domready');
-import {select} from 'd3-selection';
-import {scaleLinear} from 'd3-scale';
-import {arc, pie} from 'd3-shape';
-import {interpolateRgb} from 'd3-interpolate';
 
 domReady(() => {
   // this is just one example of how to import data. there are lots of ways to do it!
   fetch('./data/cta_data_avg.json')
     .then(response => response.json())
+    .then(data => myVis(data));
+
+  fetch('./data/cta_data_avg.json')
+    .then(response => response.json())
     .then(data => phaseDiagram(data));
 });
-  
+
+function myVis(data) {
+  // The posters will all be 24 inches by 36 inches
+  // Your graphic can either be portrait or landscape, up to you
+  // the important thing is to make sure the aspect ratio is correct.
+
+  // portrait
+  const width = 5000;
+  const height = 36 / 24 * width;
 
   // landscape
   // const height = 5000;
@@ -35,3 +44,5 @@ domReady(() => {
   const barSvg = svg.append('g').attr('transform', 'translate(0, 450)');
   barVis(barSvg, data, width, height - 450);
 
+  phaseDiagram(data, svg);
+}
--- src/phase.js
+++ src/phase.js
@@ -1,45 +1,49 @@
-//uses './data/data.json'
-function phaseDiagram(data) {
-    const width = 2000;
-    const height = 2000;
-    const centerY = 500;
-    const centerX = 1000;
-  
-  
-    const weekday = [1, 2, 3, 4, 5];
-    const weekend = [6, 7];
-    
-    const weekday_data = data.filter(d => weekday.includes(d.dow));
-    const weekend_data = data.filter(d => weekend.includes(d.dow));
-    const m_data = data.filter(d => d.dow == 1);
-    const t_data = data.filter(d => d.dow == 2);
-    const w_data = data.filter(d => d.dow == 3);
-    const h_data = data.filter(d => d.dow == 4);
-    const f_data = data.filter(d => d.dow == 5);
-    const s_data = data.filter(d => d.dow == 6);
-    const u_data = data.filter(d => d.dow == 7);
-    const filterss = [weekday_data, weekend_data, m_data, t_data, w_data, h_data, f_data, s_data, u_data, data];
-    const label = ['Weekday', 'Weekend', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun', 'All'];
-  
-    const fillScale = scaleLinear().domain([0, 349810]).range([0, 500]);
-    const color = scaleLinear().domain([0, 100]).range(['#0000FF', '#FF0000']).interpolate(interpolateRgb);
-  
-    for (var j=0; j<10; j++) {
-      const svg = select('body')
-      .append('svg')
+import {scaleLinear} from 'd3-scale';
+import {interpolateRgb} from 'd3-interpolate';
+import {select} from 'd3-selection';
+import {arc, pie} from 'd3-shape';
+
+// uses './data/data.json'
+export function phaseDiagram(data) {
+  const width = 2000;
+  const height = 2000;
+  const centerY = 500;
+  const centerX = 1000;
+
+  const weekday = [1, 2, 3, 4, 5];
+  const weekend = [6, 7];
+
+  const weekdayData = data.filter(d => weekday.includes(d.dow));
+  const weekendData = data.filter(d => weekend.includes(d.dow));
+  const mData = data.filter(d => d.dow === 1);
+  const tData = data.filter(d => d.dow === 2);
+  const wData = data.filter(d => d.dow === 3);
+  const hData = data.filter(d => d.dow === 4);
+  const fData = data.filter(d => d.dow === 5);
+  const sData = data.filter(d => d.dow === 6);
+  const uData = data.filter(d => d.dow === 7);
+  const filterss = [weekdayData, weekendData,
+    mData, tData, wData, hData, fData, sData, uData, data];
+  const label = ['Weekday', 'Weekend', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun', 'All'];
+
+  const fillScale = scaleLinear().domain([0, 349810]).range([0, 500]);
+  const color = scaleLinear().domain([0, 100]).range(['#0000FF', '#FF0000']).interpolate(interpolateRgb);
+
+  for (let j = 0; j < 10; j = j + 1) {
+    const svg = select('.vis-container');
+
+    const g = svg.append('g')
+      .attr('transform', `translate(${centerX}, ${centerY})`)
       .attr('width', width)
       .attr('height', height);
-  
-    const g = svg.append('g')
-      .attr('transform', `translate(${centerX}, ${centerY})`);
-  
+
     const arcH = arc()
       .innerRadius(0)
       .outerRadius(d => fillScale(d.data.ridership));
-      
+
     const pieH = pie()
       .value(d => d.value);
-  
+
     const arcs = g.selectAll('slice')
       .data(pieH(filterss[j]))
         .enter()
@@ -49,11 +53,10 @@ function phaseDiagram(data) {
     arcs.append('path')
       .attr('fill', d => color(d.data.temp))
       .attr('d', arcH);
-  
+
     svg.append('text')
       .attr('x', 1000)
       .attr('y', 1000)
       .text(label[j]);
-
-    } 
+  }
 }
